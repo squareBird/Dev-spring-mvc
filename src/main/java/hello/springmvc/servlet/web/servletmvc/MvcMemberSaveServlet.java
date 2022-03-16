9a01b6c80cfd1c30ef91e264dd94087351db3d2d
Lux <fails9503@gmail.com>
2022-03-16 23:53:41 +0900

    2.3 MVC의 한계 - View로 이동하는 코드가 항상 호출되어야 함 - ViewPath에 /WEB-INF/views 부분이 중복 .jsp도 항상 입력해야함 - 공통처리가 어렵다   ㅇ 기능이 복잡해질수록 공통으로 처리하는 부분이 많아질텐데 공통처리가 어려워짐   ㅇ 공통처리 한 메소드를 항상 호출해야하고 실수로 호출 안하면 문제가 생김   그럼 어떻게? -> 컨트롤러 호출 전 공통 기능을 처리   소위 수문장 역할을 하는 기능이 필요 -> 프론트 컨트롤러 패턴을 도입(입구를 하나로)   ★ 스프링 MVC의 핵심도 이 프론트 컨트롤러에 있다

--- src/main/java/hello/springmvc/servlet/web/servletmvc/MvcMemberSaveServlet.java
+++ src/main/java/hello/springmvc/servlet/web/servletmvc/MvcMemberSaveServlet.java
@@ -27,7 +27,12 @@ public class MvcMemberSaveServlet extends HttpServlet {
         // Model에 데이터를 보관한다
         request.setAttribute("member", member);
 
+        // 이부분에 중복이 많음..
+        // ViewPath에도 /WEB-INF/view가 중복되고 jsp도 항상 입력해 주어야 함
         String viewPath = "/WEB-INF/views/save-result.jsp";
+        // view로 갈때마다 이 두줄은 공통 부분인데 호출해주어야 함
+        // 이걸 메소드로 분리한다고 해도  view로 갈때마다 그 메소드를 호출해주어야 함
+        // 공통 로직을 먼저 처리해주는 프론트 컨트롤러 도입!
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
 
